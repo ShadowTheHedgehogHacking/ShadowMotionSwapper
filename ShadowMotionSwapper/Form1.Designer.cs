7b4c2956b5bc8ab0b3cb842168e33f07b308f5c0
dreamsyntax <dreamsyntax@gmail.com>
2021-04-07 00:29:32 -0700

    start winforms version, binding tests

---
+++ ShadowMotionSwapper/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace ShadowMotionSwapper {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.listBoxTarget = new System.Windows.Forms.ListBox();
+            this.listBoxDonor = new System.Windows.Forms.ListBox();
+            this.buttonMap = new System.Windows.Forms.Button();
+            this.checkBoxCopyProps = new System.Windows.Forms.CheckBox();
+            this.buttonOpenTarget = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.buttonOpenDonor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxTarget
+            // 
+            this.listBoxTarget.FormattingEnabled = true;
+            this.listBoxTarget.ItemHeight = 15;
+            this.listBoxTarget.Location = new System.Drawing.Point(12, 33);
+            this.listBoxTarget.Name = "listBoxTarget";
+            this.listBoxTarget.Size = new System.Drawing.Size(223, 274);
+            this.listBoxTarget.TabIndex = 0;
+            // 
+            // listBoxDonor
+            // 
+            this.listBoxDonor.FormattingEnabled = true;
+            this.listBoxDonor.ItemHeight = 15;
+            this.listBoxDonor.Location = new System.Drawing.Point(400, 33);
+            this.listBoxDonor.Name = "listBoxDonor";
+            this.listBoxDonor.Size = new System.Drawing.Size(237, 274);
+            this.listBoxDonor.TabIndex = 1;
+            // 
+            // buttonMap
+            // 
+            this.buttonMap.Location = new System.Drawing.Point(270, 96);
+            this.buttonMap.Name = "buttonMap";
+            this.buttonMap.Size = new System.Drawing.Size(75, 23);
+            this.buttonMap.TabIndex = 2;
+            this.buttonMap.Text = "Map";
+            this.buttonMap.UseVisualStyleBackColor = true;
+            this.buttonMap.Click += new System.EventHandler(this.buttonMap_Click);
+            // 
+            // checkBoxCopyProps
+            // 
+            this.checkBoxCopyProps.AutoSize = true;
+            this.checkBoxCopyProps.Location = new System.Drawing.Point(270, 145);
+            this.checkBoxCopyProps.Name = "checkBoxCopyProps";
+            this.checkBoxCopyProps.Size = new System.Drawing.Size(87, 19);
+            this.checkBoxCopyProps.TabIndex = 3;
+            this.checkBoxCopyProps.Text = "Copy Props";
+            this.checkBoxCopyProps.UseVisualStyleBackColor = true;
+            // 
+            // buttonOpenTarget
+            // 
+            this.buttonOpenTarget.Location = new System.Drawing.Point(63, 355);
+            this.buttonOpenTarget.Name = "buttonOpenTarget";
+            this.buttonOpenTarget.Size = new System.Drawing.Size(108, 24);
+            this.buttonOpenTarget.TabIndex = 4;
+            this.buttonOpenTarget.Text = "Open Target";
+            this.buttonOpenTarget.UseVisualStyleBackColor = true;
+            this.buttonOpenTarget.Click += new System.EventHandler(this.buttonOpenTarget_Click);
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(258, 355);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(75, 23);
+            this.buttonExport.TabIndex = 5;
+            this.buttonExport.Text = "Export";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // buttonOpenDonor
+            // 
+            this.buttonOpenDonor.Location = new System.Drawing.Point(419, 355);
+            this.buttonOpenDonor.Name = "buttonOpenDonor";
+            this.buttonOpenDonor.Size = new System.Drawing.Size(98, 23);
+            this.buttonOpenDonor.TabIndex = 6;
+            this.buttonOpenDonor.Text = "Open Donor";
+            this.buttonOpenDonor.UseVisualStyleBackColor = true;
+            this.buttonOpenDonor.Click += new System.EventHandler(this.buttonOpenDonor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 450);
+            this.Controls.Add(this.buttonOpenDonor);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.buttonOpenTarget);
+            this.Controls.Add(this.checkBoxCopyProps);
+            this.Controls.Add(this.buttonMap);
+            this.Controls.Add(this.listBoxDonor);
+            this.Controls.Add(this.listBoxTarget);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxTarget;
+        private System.Windows.Forms.ListBox listBoxDonor;
+        private System.Windows.Forms.Button buttonMap;
+        private System.Windows.Forms.CheckBox checkBoxCopyProps;
+        private System.Windows.Forms.Button buttonOpenTarget;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Button buttonOpenDonor;
+    }
+}
+
---
+++ ShadowMotionSwapper/Form1.cs
@@ -0,0 +1,94 @@
+﻿using MTPLib;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ShadowMotionSwapper {
+    public partial class Form1 : Form {
+
+        private MotionPackage targetPackage, donorPackage;
+        
+        BindingSource targetBinding = new BindingSource();
+        BindingSource donorBinding = new BindingSource();
+
+        public Form1() {
+            InitializeComponent();
+            targetBinding.DataSource = targetPackage;
+            listBoxTarget.DataSource = targetBinding;
+            listBoxTarget.DisplayMember = "Entries";
+        }
+
+        private void buttonOpenTarget_Click(object sender, EventArgs e) {
+            var dialog = new Ookii.Dialogs.WinForms.VistaOpenFileDialog {
+                Filter = "Motion files (*.mtp)|*.mtp|All files (*.*)|*.*"
+            };
+            if (dialog.ShowDialog() != DialogResult.OK) {
+                return;
+            }
+            if (!dialog.FileName.ToLower().EndsWith(".mtp")) {
+                MessageBox.Show("Pick an 'MTP' file", "Try Again");
+                return;
+            }
+            var data = File.ReadAllBytes(dialog.FileName);
+            targetPackage = MotionPackage.FromMtp(data);
+            //displayTargetPackage = CollectionViewSource.GetDefaultView(targetPackage.Entries);
+            //listBoxTarget.ItemsSource = displayTargetPackage;
+            targetBinding.DataSource = targetPackage;
+            targetBinding.DataMember = "Entries";
+
+            //listBoxTarget.DataSource = new BindingList<ManagedAnimationEntry>(targetPackage.Entries);
+        }
+
+        private void buttonOpenDonor_Click(object sender, EventArgs e) {
+            var dialog = new Ookii.Dialogs.WinForms.VistaOpenFileDialog {
+                Filter = "Motion files (*.mtp)|*.mtp|All files (*.*)|*.*"
+            };
+            if (dialog.ShowDialog() != DialogResult.OK) {
+                return;
+            }
+            if (!dialog.FileName.ToLower().EndsWith(".mtp")) {
+                MessageBox.Show("Pick an 'MTP' file", "Try Again");
+                return;
+            }
+            var data = File.ReadAllBytes(dialog.FileName);
+            donorPackage = MotionPackage.FromMtp(data);
+            //displayDonorPackage = CollectionViewSource.GetDefaultView(donorPackage.Entries);
+            //listBoxDonor.ItemsSource = displayDonorPackage;
+            //listBoxDonor.DataSource = new BindingList<ManagedAnimationEntry>(donorPackage.Entries);
+
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e) {
+            if (targetPackage == null)
+                return;
+            var dialog = new Ookii.Dialogs.WinForms.VistaSaveFileDialog();
+            if (dialog.ShowDialog() != DialogResult.OK) {
+                return;
+            }
+            if (dialog.FileName != "") {
+                File.WriteAllBytes(dialog.FileName, targetPackage.ToMtp());
+            }
+        }
+
+        private void buttonMap_Click(object sender, EventArgs e) {
+            if (listBoxTarget.SelectedIndex == -1 || listBoxDonor.SelectedIndex == -1)
+                return;
+            var targetEntry = targetPackage.Entries[listBoxTarget.SelectedIndex];
+            var donorEntry = donorPackage.Entries[listBoxDonor.SelectedIndex];
+
+            // TODO: Add additional conditionals to allow:
+            // - Copy name from donor (generally bad idea)
+
+            if (checkBoxCopyProps.Checked == true) {
+                /*targetPackage.Entries[listBoxTarget.SelectedIndex] = new ManagedAnimationEntry(targetEntry.FileName, donorEntry.FileData, donorEntry.Tuples);*/
+                targetPackage.Entries[listBoxTarget.SelectedIndex] = new ManagedAnimationEntry(donorEntry.FileName, donorEntry.FileData, donorEntry.Tuples);
+            } else {
+                targetPackage.Entries[listBoxTarget.SelectedIndex] = new ManagedAnimationEntry(targetEntry.FileName, donorEntry.FileData, targetEntry.Tuples);
+            }
+
+            //listBoxTarget.Data
+            //displayTargetPackage.Refresh();
+        }
+    }
+}
